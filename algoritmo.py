73315e06c9f267f9b180fa6ad34b726b5db678fd
ryansstudent <144918673+ryansstudent@users.noreply.github.com>
2023-11-29 10:15:22 -0300

    Add files via upload

---
+++ algoritmo.py
@@ -0,0 +1,11 @@
+
+nota1 =float(input('Escreva a primeira nota'))
+nota2 =float(input('Escreva a segunda nota'))
+nota3 =float(input('Escreva a terceira nota'))
+nota4 = float(input('Escreva a quarta nota'))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print(media)
+
+
